9eb55ca139312bf86a6ef390800b09b1481c96fe
markhaynes456 <markhaynes456@gmail.com>
2014-09-28 16:37:07 -0400

    added post creation tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe("Creating") do
+		
+	before(:each) do
+			@post
+		end
+
+		it("success new post") do
+			@post = Post.new(title: "new post", slug: "slimey slimey", text: "i am the real deal a full post")
+			@post.save
+			expect( Post.find(@post.id) ).to( be_a(Post) ) 
+		end
+
+
+		it("failed error creating post no information") do
+			@post = Post.new
+			@post.save
+			expect( Post.find(@post.id) ).to_not( be_a(Post) )
+		end
+
+		after(:each) do
+			@post.destroy
+		end
+
+	end
 end
