929c432fc5e0f06fb6b796a742640f737841e6f4
markhaynes456 <markhaynes456@gmail.com>
2014-09-28 16:56:06 -0400

    red -> green post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+
+	validates :title, :slug, :text, presence:true
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-	describe("Creating") do
+	describe("creation") do
 		
-	before(:each) do
+		before(:each) do
 			@post
 		end
 
 		it("success new post") do
 			@post = Post.new(title: "new post", slug: "slimey slimey", text: "i am the real deal a full post")
 			@post.save
-			expect( Post.find(@post.id) ).to( be_a(Post) ) 
+			expect( Post.find_by(id:@post.id) ).to( be_a(Post) ) 
 		end
 
 
 		it("failed error creating post no information") do
 			@post = Post.new
 			@post.save
-			expect( Post.find(@post.id) ).to_not( be_a(Post) )
+			expect( Post.find_by(id:@post.id) ).to_not( be_a(Post) )
 		end
 
 		after(:each) do
