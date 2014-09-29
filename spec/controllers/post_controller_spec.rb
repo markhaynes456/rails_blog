0eda46ea7e974a2e95aaee4e2fca365b792cf6c1
markhaynes456 <markhaynes456@gmail.com>
2014-09-29 18:20:42 -0400

    Capybara problem fix tommorow

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe PostController, :type => :controller do
+RSpec.describe PostsController, :type => :controller do
+	
+	describe("index page") do
+		
+		before(:all) do
+			@posts = [Post.new(title: "title1", slug: "slug1", text: "text1"),Post.new(title: "title2", slug: "slug2", text: "text2"),Post.new(title: "title3", slug: "slug1", text: "text3")]
+			@posts.each do |post|
+				post.save
+			end
+		end
+
+		it("Should list all posts") do
+			visit('/posts/')
+			expect(page).to( have_content('title2') ) and expect(page).to( have_content('slug2') ) and expect(page).to( have_content('text3')	)	
+		end		
+
+		after(:all) do
+			@posts.each do |post|
+				post.destroy
+			end
+		end
+
+	end
 
 end
