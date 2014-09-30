e05acba0d9851fda2711a61150a7555bf814840c
markhaynes456 <markhaynes456@gmail.com>
2014-09-30 17:09:45 -0400

    added features capybara fixed

---
+++ spec/features/posts.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe("Post tests") do
+
+	describe("index") do
+		before(:all) do
+      @posts = [Post.new(title: "title1", slug: "slug1", text: "text1"),Post.new(title: "title2", slug: "slug2", text: "text2"),Post.new(title: "title3", slug: "slug1", text: "text3")]
+      @posts.each do |post|
+        post.save
+      end
+    end
+
+    it("Should list all posts") do
+      visit('/posts/')
+      expect(page).to( have_content('title2') ) and expect(page).to( have_content('slug2') ) and expect(page).to( have_content('text3') )
+    end
+
+    after(:all) do
+      @posts.each do |post|
+        post.destroy
+      end
+    end
+
+	end
+
+end
