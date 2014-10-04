c99eef6e398a74107796a35051eca8266b14d0f7
markhaynes456 <markhaynes456@gmail.com>
2014-10-04 16:10:05 -0400

    Added show to post

--- spec/features/posts.rb
+++ spec/features/posts.rb
@@ -2,25 +2,38 @@ require 'rails_helper'
 
 describe("Post tests") do
 
-	describe("index") do
-		before(:all) do
-      @posts = [Post.new(title: "title1", slug: "slug1", text: "text1"),Post.new(title: "title2", slug: "slug2", text: "text2"),Post.new(title: "title3", slug: "slug1", text: "text3")]
-      @posts.each do |post|
-        post.save
-      end
+	before(:all) do
+    @posts = [Post.new(title: "title1", slug: "slug1", text: "text1"),Post.new(title: "title2", slug: "slug2", text: "text2"),Post.new(title: "title3", slug: "slug1", text: "text3")]
+    @posts.each do |post|
+       post.save
     end
+  end
+
+	describe("index page") do
 
     it("Should list all posts") do
       visit('/posts/')
       expect(page).to( have_content('title2') ) and expect(page).to( have_content('slug2') ) and expect(page).to( have_content('text3') )
     end
 
-    after(:all) do
-      @posts.each do |post|
-        post.destroy
-      end
-    end
+	end
+
+	describe("show") do
+
+		it("should show the post") do
+			visit("/posts/#{@posts[0].id}")
+			expect(page).to(have_content(@posts[0].title))
+		end
 
 	end
 
+	describe("Add a post") do
+		
+	end
+
+	after(:all) do
+    @posts.each do |post|
+   		post.destroy
+  	end
+	end
 end
