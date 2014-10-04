6daade1bdceb9c2df13d4b3452b0076f28619cf9
markhaynes456 <markhaynes456@gmail.com>
2014-10-04 17:02:57 -0400

    added new.html.erb and started working on create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,29 @@ class PostsController < ApplicationController
 		@post = Post.find_by(id:params['id'])
 	end	
 
+	def new
+		@post = Post.new
+	end
+
+	def edit
+
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to(action: :index)
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	def post_params
+		params.require(:post).permit(:title,:slug,:text)
+	end
 end
